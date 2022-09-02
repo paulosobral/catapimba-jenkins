8b8d151f18140e0ad27ddd2c5905cb2b4c01ba4b
Paulo Sobral <paulo.sobral@outlook.com.br>
2022-09-01 22:47:03 -0300

    feat: user_data.sh install docker, build image and run jenkins container with jenkins password in aws ssm parameter

--- output.tf
+++ output.tf
@@ -1,4 +1,4 @@
-output "aws_eip" {
-  value       = aws_eip.jenkins-eip.public_ip
+output "jenkins_aws_elastic_ip" {
+  value       = "http://${aws_eip.jenkins-eip.public_ip}"
   description = "Endereço IP público da instancia do Jenkins. Acessar com o protocolo HTTP. Exemplo: http://44.206.118.193"
 }
--- user_data.sh
+++ user_data.sh
@@ -1,30 +1,34 @@
-#!/bin/bash -xe
-exec > >(tee /var/log/user-data.log|logger -t user-data -s 2>/dev/console) 2>&1
+#!/bin/bash
 
-    # UPDATE AND INSTALL DOCKER:
-    sudo yum update -y
-    sudo yum install yum-utils docker -y
-    sudo usermod -aG docker ec2-user
+# UPDATE AND INSTALL DOCKER:
+sudo yum update -y
+sudo yum install yum-utils docker -y
+sudo usermod -aG docker ec2-user
 
-    sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-    && sudo chmod +x /usr/local/bin/docker-compose
-    && sudo chkconfig docker on
-    && sudo service docker start
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose &&
+sudo chkconfig docker on &&
+sudo service docker start &&
 
-    # BUILD NEW IMAGE AND RUN CONTAINER:
-    && cat <<EOF > Dockerfile
-    FROM jenkins/jenkins
-    USER root
-    RUN apt-get update && apt-get install wget -y
+# BUILD NEW IMAGE AND RUN CONTAINER:
+cd ~
+cat <<EOF > Dockerfile
+ FROM jenkins/jenkins
+ USER root
+ RUN apt-get update && apt-get install wget -y
 
-    ### Install Terraform ###
-    RUN wget --quiet https://releases.hashicorp.com/terraform/1.0.9/terraform_1.0.9_linux_amd64.zip \
-    && unzip terraform_1.0.9_linux_amd64.zip \
-    && mv terraform /usr/bin \
-    && rm terraform_1.0.9_linux_amd64.zip
+ ### Install Terraform ###
+ RUN wget --quiet https://releases.hashicorp.com/terraform/1.0.9/terraform_1.0.9_linux_amd64.zip \
+ && unzip terraform_1.0.9_linux_amd64.zip \
+ && mv terraform /usr/bin \
+ && rm terraform_1.0.9_linux_amd64.zip
 
-    USER jenkins
-    EOF
+ USER jenkins
+EOF
 
-    && docker build -t jenkins-server-image .
-    && docker run -d -p 80:8080 --name jenkins-pod jenkins-server-image
+sudo docker build -t jenkins-server-image . &&
+sudo docker run -d -p 80:8080 --name jenkins-pod jenkins-server-image &&
+
+# CREATE A SYSTEMS MANAGER PARAMETER WITH JENKINS ADMIN PASSWORD:
+JENKINS_PASSWORD=$(sudo docker exec -ti jenkins-pod cat /var/jenkins_home/secrets/initialAdminPassword)
+aws ssm put-parameter --region us-east-1 --name "JenkinsinitialAdminPassword" --value "$JENKINS_PASSWORD" --type String --overwrite
